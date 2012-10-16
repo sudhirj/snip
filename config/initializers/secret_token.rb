e858e3cc857320796a6104fcad39fbdf2ff68236
Sudhir Jonathan <sudhir.j@gmail.com>
2012-10-17 02:31:46 +0530

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snip::Application.config.secret_token = '005c0df6187e9c6883531bba83d70666975828c64e8f36f68b3eeb75711ff4357794b0ada79475985067765cc2e183de33eb2ab8bd9ed067ebe3237256478d68'
